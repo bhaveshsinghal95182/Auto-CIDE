6c514d1b594a53cd986e34c55b21eb16da1342f7
Bhavesh Singhal <work.bhavesh22@gmail.com>
2025-01-04 08:22:13 +0000

    expire token in 24hrs

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -3,33 +3,34 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema({
-    email:{
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        lowercase: true,
-        minLength: [6,"Email must be at least 6 characters long"],
-        maxLength: [100,"Email must be at most 100 characters long"]
-    },
-    password:{
-        type: String,
-        select: false,
-    }
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: [6, "Email must be at least 6 characters long"],
+    maxLength: [100, "Email must be at most 100 characters long"],
+  },
+  password: {
+    type: String,
+    select: false,
+  },
 });
 
+userSchema.statics.hashPassword = async function (password) {
+  return await bcrypt.hash(password, 10);
+};
 
-userSchema.statics.hashPassword = async function(password) {
-    return await bcrypt.hash(password, 10);
-}
+userSchema.methods.isValidPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
-userSchema.methods.isValidPassword = async function(password) {
-    return await bcrypt.compare(password, this.password);
-}
-
-userSchema.methods.generateJWT = async function() {
-    return await jwt.sign({email: this.email}, process.env.JWT_SECRET);
-}
+userSchema.methods.generateJWT = async function () {
+  return await jwt.sign({ email: this.email }, process.env.JWT_SECRET, {
+    expiresIn: "24h",
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 
