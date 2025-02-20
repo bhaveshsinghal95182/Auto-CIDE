a12d6f2595941f6b3feb06630400b539a172860a
Bhavesh Singhal <work.bhavesh22@gmail.com>
2025-02-20 17:48:27 +0000

    configuring port on backend

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import http from 'http';
 import app from './app.js';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
